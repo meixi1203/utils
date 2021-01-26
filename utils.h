3abed3ced0cad071749ce9b6d1903c8db6353447
zhoupenghui <>
2021-01-26 22:54:57 +0800

    add trim

--- utils.h
+++ utils.h
@@ -53,6 +53,21 @@ inline std::string trim(const std::string& str) {
     return str.substr(start, end - start + 1);
 }
 
+inline std::string trim_(const std::string &s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && isspace(*it)) {
+        it++;
+    }
+
+    std::string::const_reverse_iterator rit = s.rbegin();
+    while (rit.base() != it && isspace(*rit)) {
+        rit++;
+    }
+
+    return std::string(it, rit.base());
+}
+
 inline std::string::size_type sep_size(const std::string& s) {
     return s.size();
 }
